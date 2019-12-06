535e2edf321c91f139bcfff601498c9e1c137d0c
Neil Mispelaar <neil.mispelaar@outlook.com>
2019-12-06 15:46:39 -0500

    Initial configuration for coming soon page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 // tailwind.config.js
 module.exports = {
+	theme: {
+		extend: {
+			colors: { 
+				'brand-orange': '#f9f9f9', 
+				'brand-green': '#246428',
+				'brand-blue': '#441498'
+			} 
+		}
+	},
+	variants: {},
+
 	plugins: [
-	  //require('tailwindcss-transforms'),
-	  //require('tailwindcss-transitions'),
-	  //require('tailwindcss-border-gradients'),
+		//require('tailwindcss-transforms'),
+		//require('tailwindcss-transitions'),
+		//require('tailwindcss-border-gradients'),
 	],
-  }
+}
