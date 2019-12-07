cac54bad3f938bc929fad8cd28057fd4f546d951
Neil Mispelaar <neil.mispelaar@outlook.com>
2019-12-07 00:13:51 -0500

    Update postcss config file

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,13 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
-
 	plugins: [
-		// ...
 		require('tailwindcss'),
-
-		purgecss({
-		content: ['./**/*.html']
+		require('autoprefixer'),
+		process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+			content: [
+				'./src/**/*.vue',
+				'./public/index.html',
+			],
+			defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 		})
-		// ...
 	]
-
 }
